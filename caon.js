beac3d7cb35a1cbbafe6e1d4258eda817ebcf360
faisalmhm00 <faisalmhm00@gmail.com>
2017-06-29 19:30:14 +0700

    Update caon.js

--- caon.js
+++ caon.js
@@ -23,7 +23,7 @@ if(x=="1"){xxx="/2017/06/boruto-the.html"}
 if(x=="2"){xxx="/2017/06/game-pc.html}
 if(x=="3"){xxx="/2017/06/gmail.html}
 if(x=="4"){xxx="/2017/06/google-drive.html"}
-if(x=="5"){xxx="/2017/06/anime.html}a_to_ve[i].href="http://percobaansingkat.blogspot.co.id/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/06/anime.html}a_to_ve[i].href="http://percobaansingkat.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
